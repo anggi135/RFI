3789977f132c7a81d517e63595a882921d4669d3
Anggi Prayitno <40010260+anggi135@users.noreply.github.com>
2024-12-08 15:50:25 -0500

    Create bounty.js

---
+++ bounty.js
@@ -0,0 +1,5 @@
+console.log('Skrip dari evil.js telah dijalankan!');
+alert('Skrip telah dimuat dengan nonce!');
+const newDiv = document.createElement('div');
+newDiv.textContent = 'Skrip evil.js berhasil dimuat!';
+document.body.appendChild(newDiv);
